f48cd7b3ebe3b2b1651f28ecc6915391f641eabd
FRAZ <faslam7646@gmail.com>
2023-04-05 20:51:18 -0400

    created some base components and made sure they were linked to main file

---
+++ cv-project/src/components/work.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Work extends Component {
+  render() {
+    return (
+      <div>
+            <h1>This is the work component </h1>
+      </div>
+    )
+  }
+}
+
+export default Work
--- cv-project/src/index.js
+++ cv-project/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import General from './components/general';
+import Education from './components/education';
+import Work from './components/work';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <General />
+    <Education />
+    <Work />
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
